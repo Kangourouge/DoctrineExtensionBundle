b5044ec8d43168627df9189ad7316a32ca873300
aminiconi <aminiconi@kangourouge.com>
2019-03-21 10:53:10 +0100

    [U] MinMaxRangeType

--- Form/Type/MinMaxRangeType.php
+++ Form/Type/MinMaxRangeType.php
@@ -5,12 +5,15 @@ namespace KRG\DoctrineExtensionBundle\Form\Type;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class MinMaxRangeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $data = $builder->getData();
+
         $builder
             ->add($builder->getName().'_min', TextType::class, [
                 'label'     => false,
@@ -32,6 +35,24 @@ class MinMaxRangeType extends AbstractType
                 'required'  => false,
                 'data'      => (string)$data['max'],
             ]);
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'rebindMinMax']);
+    }
+
+    /**
+     * Rebind form data if submitted request data is different from config
+     */
+    public function rebindMinMax(FormEvent $event)
+    {
+        $data = $event->getData();
+        $form = $event->getForm();
+
+        $data['price_min'] = $form->getData()['min'];
+        $data['price_max'] = $form->getData()['max'];
+        $data['price_from'] = $form->getData()['min'];
+        $data['price_to'] = $form->getData()['max'];
+
+        $event->setData($data);
     }
 
     public function getBlockPrefix()
